213ea5f9037cd21e1405599bb8a91ed4aee74209
BlunderSaint <parasprez@gmail.com>
2026-04-25 14:24:57 +0530

    MotherAnimationsUpdate

--- Assets/Paras/Script/DialogueManager.cs
+++ Assets/Paras/Script/DialogueManager.cs
@@ -1,16 +1,25 @@
-using UnityEngine;
+//using UnityEngine;
 
-public class DialogueManager : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+//public class DialogueManager : MonoBehaviour
+//{
+//    [Header("Dialogue Settings")]
+//    public string speakerName = "Rogue";
+//    [TextArea(3, 10)]
+//    public string[] dialogueLines;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+//    // This boolean remembers if the text has been shown
+//    private bool hasPlayed = false;
+//    private bool hasEnded = false;
+//    private void OnTriggerEnter2D(Collider2D collision)
+//    {
+//        // 1. Did the player step inside?
+//        // 2. Has this NOT played yet?
+//        if (collision.CompareTag("Player") && !hasPlayed)
+//        {
+//            hasPlayed = true; // Mark it as played immediately!
+
+//            // Send the text to the UI
+//            DialogueManager.instance.StartDialogue(speakerName, dialogueLines);
+//        }
+//    }
+//}
